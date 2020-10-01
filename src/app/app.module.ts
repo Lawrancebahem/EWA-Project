1b49b38b436af4df2ea28893262ee5ff56c85cd4
lawrancebahem <lawrancebahem@gmail.com>
2020-10-01 16:26:06 +0200

    -main page components have been added: header, nav-bar- and home --bootstrap has been installed and applied in de angular.json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/mainpage/header/header.component';
+import { HomeComponent } from './components/mainpage/home/home.component';
+import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
