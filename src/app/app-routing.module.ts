1fe88b928dd71c52e6b5b05353ad5aec58de4c0e
lawrancebahem <lawrancebahem@gmail.com>
2020-10-08 00:52:26 +0200

    log in page has been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent} from "./components/mainpage/register/register.component";
+import {LoginComponent} from "./components/mainpage/login/login.component";
+import {HomeComponent} from "./components/mainpage/home/home.component";
+import {NavBarComponent} from "./components/mainpage/nav-bar/nav-bar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"register", component: RegisterComponent},
+  {path:"login", component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
