9d3e44a2f3dfa5c26e51b4f7267294cc107b5acc
lawrancebahem <lawrancebahem@gmail.com>
2020-10-07 23:56:13 +0200

    Files changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/mainpage/header/header.component';
-import { HomeComponent } from './components/mainpage/home/home.component';
-import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
-import { BannerComponent } from './components/HomePage/bannerHomePage/banner.component';
-import { ZoekbalkHomePageComponent } from './components/HomePage/search-home-page/zoekbalk-home-page.component';
-import { ActivitiesHomePageComponent } from './components/HomePage/activities-home-page/activities-home-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/mainpage/header/header.component';
+import {HomeComponent} from './components/mainpage/home/home.component';
+import {NavBarComponent} from './components/mainpage/nav-bar/nav-bar.component';
+import {BannerComponent} from './components/HomePage/bannerHomePage/banner.component';
+import {ZoekbalkHomePageComponent} from './components/HomePage/search-home-page/zoekbalk-home-page.component';
+import {ActivitiesHomePageComponent} from './components/HomePage/activities-home-page/activities-home-page.component';
+import {RegisterComponent} from "./components/mainpage/register/register.component";
+import {LoginComponent} from "./components/mainpage/login/login.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    NavBarComponent,
-    BannerComponent,
-    ZoekbalkHomePageComponent,
-    ActivitiesHomePageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        NavBarComponent,
+        BannerComponent,
+        ZoekbalkHomePageComponent,
+        ActivitiesHomePageComponent,
+        RegisterComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/mainpage/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/mainpage/nav-bar/nav-bar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {NavBarComponent} from './nav-bar.component';
 
-import { NavBarComponent } from './nav-bar.component';
+
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
