563099823f9967b3f821f89570481790cca03f1e
Moustafa <moustafa.fadil@hva.nl>
2020-10-20 11:31:29 +0200

    kleur veranderd van pagina's en path toegevoegd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {RegisterComponent} from "./components/mainpage/register/register.compone
 import {LoginComponent} from "./components/mainpage/login/login.component";
 import {HomeComponent} from "./components/HomePage/home/home.component";
 import {ActivitywandelenComponent} from "./components/activitypages/activitywandelen/activitywandelen.component";
+import {CategorypageComponent} from "./components/categorypage/categorypage/categorypage.component";
 
 const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
   {path:'login', component: LoginComponent},
   {path:'register', component:RegisterComponent},
-  {path:'activities', component:ActivitywandelenComponent}
+  {path:'activities', component:ActivitywandelenComponent},
+  {path:'category', component:CategorypageComponent}
 ];
 
 @NgModule({
