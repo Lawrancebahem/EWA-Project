7823beba6e08d3294d09d9be63bd511c6066f099
Tico Vermeer <tico.vermeer@hva.nl>
2020-10-23 17:33:24 +0200

    added list with categories to activity-overview

--- src/app/components/activityOverview/activity-overview/activity-overview.component.ts
+++ src/app/components/activityOverview/activity-overview/activity-overview.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 // @ts-ignore
-import  *  as  activities  from '../../../json/activities.json';
+import *  as  activities from '../../../json/activities.json';
 
 
 @Component({
-  selector: 'app-activity-overview',
-  templateUrl: './activity-overview.component.html',
-  styleUrls: ['./activity-overview.component.css']
+    selector: 'app-activity-overview',
+    templateUrl: './activity-overview.component.html',
+    styleUrls: ['./activity-overview.component.css']
 })
 export class ActivityOverviewComponent implements OnInit {
-  activityArray:  any  = (activities  as  any).default;
+    activityArray: any = (activities as any).default;
+    categoryArray: Array<String> = ['herfst', 'thuis'];
 
+    constructor() {
+    }
 
-  constructor() { }
+    ngOnInit(): void {
+        this.getAllCategories()
+    }
 
-  ngOnInit(): void {
-    console.log(this.activityArray)
-  }
+    getAllCategories(): any {
+        for (let activity of this.activityArray) {
+            for (let i = 0; i < activity.categories.length; i++) {
+                if (!this.categoryArray.includes(activity.categories[i])) {
+                    this.categoryArray.push(activity.categories[i])
+                }
+            }
+        }
+    }
 }
+
