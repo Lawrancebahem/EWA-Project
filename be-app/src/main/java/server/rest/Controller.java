2f0358f41b8096f933235e1652a1b1025431b1e2
lawrancebahem <lawrancebahem@gmail.com>
2020-10-28 11:02:08 +0100

    spring-boot added files

---
+++ be-app/src/main/java/server/rest/Controller.java
@@ -0,0 +1,17 @@
+package server.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/info")
+public class Controller {
+
+    @GetMapping
+    public String SayHello(){
+        return "Helloo";
+    }
+
+}
