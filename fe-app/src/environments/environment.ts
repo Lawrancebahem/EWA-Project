2f8634a03c0b77f1f17d993f3cd7e9a635459215
lawrancebahem <lawrancebahem@gmail.com>
2020-10-28 01:39:27 +0100

    updated

--- fe-app/src/environments/environment.ts
+++ fe-app/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apirUrl : "http://localhost:8080",
+  apirUrl : "http://localhost:8080/rest",
   environmentName:'development',
   production: false
 };
