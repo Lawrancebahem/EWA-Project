5dd76fd3d446457c28800bd893ed46d276c176ea
Lawrance <Lawrance.bahem@hva.nl>
2020-12-16 15:26:29 +0100

    -edited the environment

--- fe-app/src/environments/environment.prod.ts
+++ fe-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apirUrl : "https://digital-life-backend-prod.herokuapp.com",
+  apiUrl : "https://digital-life-backend-prod.herokuapp.com",
   environmentName: "production",
   production: true
 };
