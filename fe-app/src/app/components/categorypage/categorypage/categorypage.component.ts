d9d766f9fc80965107ad6bf1942c5d83f55ef264
Tico Vermeer <tico.vermeer@hva.nl>
2020-12-28 13:28:58 +0100

    categorypage bugfix

--- fe-app/src/app/components/categorypage/categorypage/categorypage.component.ts
+++ fe-app/src/app/components/categorypage/categorypage/categorypage.component.ts
@@ -57,6 +57,8 @@ export class CategorypageComponent implements OnInit {
                     title: activity[5]
                 }
             })
+            console.log(this.activityArray);
+
         }, error => {
             console.log(error)
         })
